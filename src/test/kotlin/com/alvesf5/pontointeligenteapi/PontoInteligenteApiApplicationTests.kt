6379c206ad7d929e2754dd64be3c812445ef85e4
AlvesF5 <matheus.cruz_@hotmail.com>
2022-02-14 16:48:23 -0300

    Commit inicial

---
+++ src/test/kotlin/com/alvesf5/pontointeligenteapi/PontoInteligenteApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alvesf5.pontointeligenteapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontoInteligenteApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
